ca72a8454047d312ed99c50acf95ad1efda63415
Freezinghot <58262567+Freezinghot@users.noreply.github.com>
2019-11-29 18:29:35 +0800

    Create learn_tuple.py
    学习tuple元组数据类型操作

---
+++ learn_tuple.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @File  : learm_tuple.py
+# @Author: Freezinghot
+# @Date  : 2019/11/29
+# @Desc  : 学习元组类型tuple--与列表类似，但元组里的元素不能被修改
+##初始化一个tuple类型变量z
+z = ("a","b","c","d","e")
+print (z)
+##读取z中的元素，与list类似
+print (z[0])
+print (z[-1])
+print (z[0:3])
